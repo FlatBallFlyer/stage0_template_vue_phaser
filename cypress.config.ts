fa96bc7f0a562b0d67e346a41d343e224d22a43e
Mike Storey <mike@agile-learning.institute>
2026-02-16 15:06:57 -0500

    Port fix for cypress testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8185',
+    baseUrl: 'http://localhost:{{repo.port}}',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: false,
